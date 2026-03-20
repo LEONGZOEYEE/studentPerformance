b8a407361d9ac6f1e2ec64f827fa527c7fb655d6
Leong Zoe Yee <leong061005@gmail.com>
2026-03-21 03:52:31 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ import pandas as pd
 import numpy as np
 import streamlit as st
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder, StandardScaler
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, confusion_matrix
 from sklearn.svm import SVC
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.neural_network import MLPClassifier
